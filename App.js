3d8f24bb8685906c10f895f9f6ec187d15eeca1b
Josue-Santos-Pinto <js.pinto55@gmail.com>
2022-07-12 12:54:56 -0300

    Calculadora Completa

---
+++ App.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect} from "react";
+import styled from 'styled-components/native'
+import { Text, View, Button } from "react-native";
+
+const Page = styled.SafeAreaView`
+  flex:1;
+  align-items: center;
+  margin-top: 20px;
+  
+`
+const HeaderText = styled.Text`
+  font-size: 25px;
+`
+
+const Input = styled.TextInput`
+  width: 90%;
+  height: 40px;
+  font-size: 18px;
+  background-color: #EEE;
+  margin-top: 20px;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom:20px;
+`
+
+const CalcButton = styled.Button`
+  margin-top: 10px;
+`
+
+const ResultArea = styled.View`
+  width: 100%;
+  margin-top: 30px;
+  background-color: #EEE;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`
+
+const ResultItemTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const ResultItem = styled.Text`
+  font-size: 15px;
+  margin-bottom: 30px;
+`
+
+const PctArea = styled.View`
+  width: 90%;
+  flex-direction: row;
+  margin: 20px;
+  justify-content: space-around;
+`
+const PctItem = styled.Button`
+  
+`
+
+
+
+export default () => {
+
+  const [bill, setBill] = useState("")
+  const [tip, setTip] = useState(0)
+  const [pct, setPct] = useState(10)
+
+
+  const calc = () => {
+    let nBill = parseFloat(bill) 
+
+    if(nBill){
+      setTip(nBill * (pct/100))
+    } 
+  }
+
+
+  useEffect(()=>{
+    calc()
+  },[pct,bill])
+
+  return (
+    <Page >
+      <HeaderText> Calculadora de Gorjeta </HeaderText>
+      <Input
+      placeholder='Quanto deu a conta ?'
+      placeholderTextColor='#000'
+      keyboardType='numeric'
+      value={bill}
+      onChangeText={n=>setBill(n)}
+       />
+       <PctArea>
+          <PctItem title='5%' onPress={()=>setPct(5)}/>
+          <PctItem title='10%' onPress={()=>setPct(10)}/>
+          <PctItem title='15%' onPress={()=>setPct(15)}/>
+          <PctItem title='20%' onPress={()=>setPct(20)}/>
+       </PctArea>
+       
+
+       {bill != '' &&
+          <ResultArea>
+              <ResultItemTitle> Valor da Conta </ResultItemTitle>
+              <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+              <ResultItemTitle> Valor da Gorjeta </ResultItemTitle>
+              <ResultItem>R$ {parseFloat(tip).toFixed(2)} ({pct}%)</ResultItem>
+
+              <ResultItemTitle> Valor Total </ResultItemTitle>
+              <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+          </ResultArea>
+        }
+        
+    </Page>
+  )
+}
+
